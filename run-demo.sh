fbd622380bbef73c94ad075f7f77095e3da7f901
xbakhromjon <xbakhromjon@gmail.com>
2022-11-05 16:54:24 +0500

    init

---
+++ run-demo.sh
@@ -0,0 +1,2 @@
+docker build -t demo-image .
+docker run --name demo-app -p 8080:8080 -d demo-image
---
+++ src/main/java/uz/bakhromjon/DockerForJavaDevsApplication.java
@@ -0,0 +1,42 @@
+package uz.bakhromjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerForJavaDevsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerForJavaDevsApplication.class, args);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
